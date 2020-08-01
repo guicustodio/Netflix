eab18f575c406646057c39c56cf06866e8df487a
Guilherme Custodio <guiilherme_custodiio@hotmail.com>
2020-08-01 17:40:47 -0300

    teste

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import * as serviceWorker from './serviceWorker';
+import CadastroVideo from './Pages/Cadastro/Video/CadastroVideo';
+import CadastroCategoria from './Pages/Categoria/CadastroCategoria';
+
+const Pagina404 = () => (<div>Página 404</div>)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
+
+
+
